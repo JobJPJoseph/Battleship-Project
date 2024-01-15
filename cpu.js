6906fb3d2e361d4cf7f6ae94eab82f804b339467
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2024-01-15 01:13:49 -0500

    We Learned about Mochai and Chai and implemented tests for the cpu class.

--- cpu.js
+++ cpu.js
@@ -13,4 +13,22 @@ class CPU {
 
 }
 
+// const cpu = new CPU();
+
+// const available = [
+//     [0, 0], [0, 1], [0, 2]
+// ];
+
+// async function getInput() {
+//     return await cpu.getCoordinate(available);
+// }
+
+// // You need to use await when calling the async function
+// async function main() {
+//     const input = await getInput();
+//     console.log(input);
+// }
+
+// main();
+
 module.exports = CPU;
---
+++ test/cpu.spec.js
@@ -0,0 +1,71 @@
+const { expect } = require('chai');
+const  sinon  = require('sinon');
+
+const CPU = require('../cpu');
+
+describe('CPU class', function() {
+
+    it('The CPU class successfully created', function () {
+        const cpu = new CPU();
+
+        expect(cpu).to.exist
+    });
+
+    it('should be an instance of the CPU class', function () {
+        const cpu = new CPU();
+
+        expect(cpu).to.be.instanceOf(CPU);
+    });
+
+    it('should declare a method call getCoordinate', function () {
+        const cpu = new CPU();
+
+        const isMethod = cpu.getCoordinate;
+
+        expect(isMethod).to.exist;
+    })
+
+    it('getCoordinate must be asynchronous', function () {
+        const cpu = new CPU();
+
+        // We want the getCoordinate() to have an asychronous behavior.
+        const input = cpu.getCoordinate();
+
+        expect(input).to.be.a('Promise');
+    });
+
+    it('getCoordinate should return random coordinate from availablePositions', function () {
+        const cpu = new CPU();
+
+        const availablePositions = [
+            [0, 0], [0, 1], [0, 2]
+        ];
+
+        async function getInput() {
+            const input = await cpu.getCoordinate(availablePositions);
+
+            expect(availablePositions).to.deep.include(input);
+        }
+
+        getInput();
+
+    });
+
+    it('getCoordinate should use the method Math.random()', function () {
+        // I want to make sure that that the algorithm created uses Math.random
+        const cpu = new CPU();
+
+        const availablePositions = [
+            [0, 0], [0, 1], [0, 2]
+        ];
+
+        const mathRandomSpy = sinon.spy(Math, 'random');
+
+        cpu.getCoordinate(availablePositions);
+
+        expect(mathRandomSpy.called).to.be.true;
+
+        mathRandomSpy.restore();
+    });
+
+});
